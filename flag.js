fa6bac83b2b07236e3b4e4347cac32d436d683ae
ruban67 <52911922+ruban67@users.noreply.github.com>
2022-06-30 17:23:04 +0530

    Add files via upload

---
+++ flag.js
@@ -0,0 +1,12 @@
+var request = new XMLHttpRequest();
+request.open('get','https://restcountries.com/v3.1/all',true);
+request.send();
+request.onload=function(){
+    var data=JSON.parse(request.response);
+
+    console.log(data);
+  for(var i=0;i<data.length;i++){
+    console.log(data[i].flags);
+  }
+ 
+}
